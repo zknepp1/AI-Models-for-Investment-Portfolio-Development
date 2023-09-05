7fe2b8b58e67ead896562fd3b03d44aa590ce1a4
Zachary <41703755+zknepp1@users.noreply.github.com>
2023-09-05 11:50:13 -0500

    Create dataframe_collector.py

---
+++ dataframe_collector.py
@@ -0,0 +1,85 @@
+# This class will be used to retrieve the financial data, and news data
+class DataFrameCollection:
+    def __init__(self):
+
+        #Connecting to my drive to grab the news dataset
+        # This will be changed when I store the datasets locally
+        from google.colab import drive
+        drive.mount('/content/drive')
+
+        # List of tickers the class is instructed to retrieve
+        self.list_of_tickers = ['OKE','VALE','MSFT','NVDA',
+                                'AMD','LTHM','ALB','SNPS',
+                                'IRM','T','PAYC','TSLA','KDP',
+                                'COIN','SNOW','AMZN','CRM','GOOGL',
+                                'LMT','^GSPC']
+
+        self.dataframes = []
+        self.end_date = datetime.now().strftime('%Y-%m-%d')
+        self.news_data = self.retrieve_news_data()
+
+    # Computes average rolling windows for a dataframe. Returns dataframe with new columns
+    def make_rolling_window(self, dataframe, window_size, size):
+        numbers_series = dataframe['Close']
+        windows = numbers_series.rolling(window_size)
+        moving_averages = windows.mean()
+        moving_averages_list = moving_averages.tolist()
+        column_name = str(size) + '_day_rolling'
+        dataframe[column_name] = moving_averages_list
+        return dataframe
+
+    # Uses yfinance to go retrieve ticker data
+    def retrieve_financial_data(self):
+        for i in self.list_of_tickers:
+            data = yf.Ticker(i)
+            hist = data.history(start='2000-01-01',end=self.end_date)
+            dataframe = pd.DataFrame(hist)
+
+            dataframe['date'] = dataframe.index
+            pattern = r'\d\d\d\d-\d\d-\d\d'
+            re_matches = []
+
+            for value in dataframe['date']:
+                value = str(value)
+                match = re.search(pattern, value)
+                if match:
+                    re_matches.append(match.group())
+                else:
+                    re_matches.append('no match')
+
+            dataframe['dt'] = re_matches
+            dataframe['dt'] = pd.to_datetime(dataframe['dt'], format='%Y-%m-%d')
+            dataframe.set_index('dt', inplace=True)
+
+
+            window_size = 5
+            window_size2 = 10
+            window_size3 = 20
+            dataframe = self.make_rolling_window(dataframe, window_size, 'five')
+            dataframe = self.make_rolling_window(dataframe, window_size2, 'ten')
+            dataframe = self.make_rolling_window(dataframe, window_size2, 'twenty')
+
+            # Right here I am determining What i am trying to predict
+            # This is the closing price for the stock 20 days ahead of time
+            dataframe['stock'] = i
+            dataframe['Target'] = dataframe['Close'].shift(-20)
+            dataframe.dropna(inplace=True)
+
+            self.dataframes .append(dataframe)
+
+
+
+    # This function retrieves the local news dataset
+    def retrieve_news_data(self):
+        df = pd.read_csv('/content/drive/My Drive/news/grouped_with_sentiment.csv')
+        df['date'] = pd.to_datetime(df['clean_dates'], format='%Y/%m/%d')
+        df.set_index('date', inplace=True)
+        return df
+
+    # Returns the news dataframe. Returns a pandas dataframe
+    def return_news_data(self):
+        return self.news_data
+
+    # Returns the financial data for all tickers specified. Returns a list item
+    def return_dataframes(self):
+        return self.dataframes
