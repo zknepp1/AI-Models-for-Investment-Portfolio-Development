94fd0e9b30cfbc5c705b7254a6add5572ffc68c6
Zachary <41703755+zknepp1@users.noreply.github.com>
2023-10-09 08:41:22 -0500

    added comments

--- code/simulation.py
+++ code/simulation.py
@@ -5,19 +5,12 @@ from sklearn.preprocessing import StandardScaler
 import numpy as np
 import pandas as pd
 
-#from dataframe_collector import DataFrameCollection
-#from news_collecter import News_Collector
-#from news_cleanup import Text_Cleaner
-#from join_data import Join_Data
-
+# This class gets the most recent month of data for each ticker, and the models for each ticker
 class Investment_Manager:
     def __init__(self, tickers):
         self.tickers_list = tickers
         self.data_list = self.loop_load_sim_data()
         self.models_list = self.loop_load_models()
-
-
-
 
         self.vars = ['Open', 'High', 'Low','Close','Volume',
                   'five_day_rolling','ten_day_rolling','twenty_day_rolling',
@@ -34,13 +27,10 @@ class Investment_Manager:
 
         self.preds = self.make_predictions()
         self.data_with_preds = self.data_with_preds()
-
         self.final_df = self.prep_for_opt()
 
 
-        #self.strategy()
-
-
+    # loops load_sim_data for each ticker
     def loop_load_sim_data(self):
         print('loop sim start')
         sim_list = []
@@ -49,13 +39,13 @@ class Investment_Manager:
         print('loop sim end')
         return sim_list
 
-
+    # Loads recent month of data
     def load_sim_data(self, tic):
         path = '/home/zacharyknepp2012/Knepp_OUDSA5900/data/'+ str(tic) +'_sim_df.csv'
         df = pd.read_csv(path)
         return df
 
-
+    # loops load_model for each ticker
     def loop_load_models(self):
         print('loop model start')
         model_list = []
@@ -64,31 +54,26 @@ class Investment_Manager:
         print('loop model end')
         return model_list
 
-
+    # loads the model
     def load_model(self, tic):
         path = '/home/zacharyknepp2012/Knepp_OUDSA5900/models/' + str(tic) + 'model'
         loaded_model = tf.keras.models.load_model(path)
         return loaded_model
 
-
+    # Makes predictions on sim data with models
     def make_predictions(self):
-        print(len(self.data_list))
         count = 0
         preds_list = []
         for df in self.data_list:
           print(df.shape)
-          print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
           X = df[self.vars]
 
           X = X.dropna()
-          print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
-          # Convert to numpy arrays
           X_array = np.array(X)
 
           scaler = StandardScaler()
           scaler.fit(X_array)
           X_scaled = scaler.transform(X_array)
-          #print(X_scaled)
           m = 35
           timesteps = 1
           X_reshaped = X_scaled.reshape(X_scaled.shape[0], timesteps, m)
@@ -97,7 +82,7 @@ class Investment_Manager:
           count += 1
         return preds_list
 
-
+    # Joins the predictions to the main dfs
     def data_with_preds(self):
         count = 0
         df_list = []
@@ -115,14 +100,12 @@ class Investment_Manager:
           count += 1
         return df_list
 
-
-
+    # Calculate the return as a percentage
     def calculate_return(self, df):
-        # Calculate the return as a percentage
         df['return'] = ((df['preds'] - df['close']) / df['close']) * 100
         return df
 
-
+    # Calculates sharpe ratio, returns sharpe_ratio, mean_return, stddev
     def calculate_sharpe_ratio(self, df):
         risk_free_rate = 0.001
         stddev = np.std(df['return'], ddof=1)
@@ -130,7 +113,7 @@ class Investment_Manager:
         sharpe_ratio = (mean_return - risk_free_rate) / stddev
         return sharpe_ratio, mean_return, stddev
 
-
+    # Joins sharpe_ratio, mean_return, stddev to main dfs
     def prep_for_opt(self):
         std_per_df = []
         return_per_df = []
@@ -155,33 +138,24 @@ class Investment_Manager:
         print(df.shape)
         return df
 
-
+    # Maximizes returns by solving a linear programming problem
     def maximize_returns(self):
-        # Create a PuLP LP problem to optimize the number of stocks to buy
         lp_problem = pulp.LpProblem("Optimize_Stocks_To_Buy", pulp.LpMaximize)
-
-        # Define decision variables for the number of shares to buy for each stock
         shares_to_buy = pulp.LpVariable.dicts("Shares_To_Buy", self.final_df['ticker'], lowBound=0, cat='Integer')
-
-        # Define the objective function (maximize expected return)
+        
+        #Objective function
         lp_problem += pulp.lpSum([self.final_df.at[i, 'return'] * shares_to_buy[self.final_df.at[i, 'ticker']] for i in self.final_df.index])
 
-        # Add a constraint to limit the total budget spent
+        # Conrstraints
         total_budget = 5000
         lp_problem += pulp.lpSum([self.final_df.at[i, 'open'] * shares_to_buy[self.final_df.at[i, 'ticker']] for i in self.final_df.index]) <= total_budget
-
-        # Add a constraint to ensure a minimum required Sharpe ratio
         risk_free_rate = 0.001
         min_sharpe_ratio = 0.1
         max_concentration = 0.05
         lp_problem += pulp.lpSum([(self.final_df.at[i, 'return'] - risk_free_rate) * shares_to_buy[self.final_df.at[i, 'ticker']] / self.final_df.at[i, 'std'] for i in self.final_df.index]) >= min_sharpe_ratio
 
-        #lp_problem +=pulp.lpSum([( shares_to_buy[stock] <= max_concentration * shares_to_buy[stock]
-
-
         # Solve the linear programming problem
         lp_problem.solve()
-
 
         # Print the number of shares to buy for each stock
         for stock in self.final_df['ticker']:
@@ -198,13 +172,4 @@ print('hello world')
 tickers = ['INTC','XEL','NEE','DD','MOS','BA','MMM','DAL','CME','TRV','V','HSY','K','PEP','SBUX','NFLX','JNJ','XOM','COP']
 l = Investment_Manager(tickers)
 l.maximize_returns()
-
-#print('THIS IS THE END...')
-
-
-
-#sim_df.to_csv('/home/zacharyknepp2012/Knepp_OUDSA5900/data/'+ str(tics[count]) +'_sim_df.csv', index=False)
-#  model.save('/home/zacharyknepp2012/Knepp_OUDSA5900/models/' + str(tics[count]) + 'model')
-
-
 
